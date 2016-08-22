de21c8815d50b0f9bce74596b8c770747ca772a7
黄健 <huangjian@huangjiandeMacBook-Air.local>
2016-08-22 14:13:40 +1200

    Initial Commit

---
+++ weather/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  weather
+//
+//  Created by 黄健 on 16/8/22.
+//  Copyright (c) 2016年 黄健. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
